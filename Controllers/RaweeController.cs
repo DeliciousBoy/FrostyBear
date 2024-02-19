e3869227c9dcd437acea847016356ed1f0be1e3b
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2024-02-19 11:18:27 +0700

    Rawee.Test

---
+++ Controllers/RaweeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostyBear.Controllers
+{
+    public class RaweeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
