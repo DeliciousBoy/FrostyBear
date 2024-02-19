24e764acd3073657f3b83384b4ab7196bd672729
ittidet <86585134+ittidet12@users.noreply.github.com>
2024-02-19 10:45:41 +0700

    ittidet add

---
+++ Controllers/IT.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostyBear.Controllers
+{
+    public class IT : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
