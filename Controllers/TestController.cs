c90a2c35c05249406e0e2f2db113bda7bf8a657c
DeliciousBoy <outdata.gide@gmail.com>
2024-02-23 22:10:06 +0700

    gide32

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -6,7 +6,7 @@ namespace FrostyBear.Controllers
     {
         public string Index()
         {
-            return "test back-end only gide3";
+            return "test back-end only gide32";
         }
     }
 }
