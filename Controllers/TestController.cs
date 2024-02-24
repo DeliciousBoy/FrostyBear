e99d633c3062d72790cb37766d94c56f6a59e7d5
DeliciousBoy <outdata.gide@gmail.com>
2024-02-24 22:47:36 +0700

    test ping

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -6,7 +6,7 @@ namespace FrostyBear.Controllers
     {
         public string Index()
         {
-            return "test back-end only1";
+            return "test back-end only";
         }
     }
 }
