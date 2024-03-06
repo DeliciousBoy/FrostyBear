7abe4d5560864dc45c5938e95884d32828547127
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2024-03-06 22:47:22 +0700

    Login shop for customer

---
+++ Controllers/ShopLoginController.cs
@@ -0,0 +1,39 @@
+﻿using FrostyBear.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FrostyBear.Controllers
+{
+    public class ShopLoginController : Controller
+    {
+        private readonly FrostyBearContext _db;
+
+        public ShopLoginController(FrostyBearContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Customer model)
+        {
+            var user = await _db.Customers.FirstOrDefaultAsync(u => u.CustomerUsername == model.CustomerUsername && u.CustomerPassword == model.CustomerPassword);
+
+            if (user != null)
+            {
+                TempData["SuccessMessage"] = "Login successful!";
+                return RedirectToAction("Index", "About"); // Redirect to the dashboard
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Incorrect username or password.";
+                return RedirectToAction("Index"); // Redirect back to the login page
+            }
+        }
+    }
+}
