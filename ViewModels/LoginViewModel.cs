d566e0abc653dae1c64068f7da6851567c9dc793
Scrambleds <96569989+Scrambleds@users.noreply.github.com>
2024-03-06 21:27:13 +0700

    Login

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FrostyBear.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "กรุณาป้อนชื่อผู้ใช้")]
+        public string EmployeeUsername { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนรหัสผ่าน")]
+        [DataType(DataType.Password)]
+        public string EmployeePassword { get; set; }
+    }
+}
+
