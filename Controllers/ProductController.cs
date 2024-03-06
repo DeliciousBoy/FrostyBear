95c0d80cadef32987050086343d54eb57e8e0042
ittidet <86585134+ittidet12@users.noreply.github.com>
2024-03-06 22:59:08 +0700

    product create,edit,delete

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using FrostyBear.Models;
 using Microsoft.AspNetCore.Mvc;
 using FrostyBear.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace FrostyBear.Controllers
 {
     public class ProductController : Controller
@@ -9,6 +10,30 @@ namespace FrostyBear.Controllers
 
         public ProductController(FrostyBearContext db)
         { _db = db; }
+
+        public IActionResult Index()
+        {
+            var pdvm = from p in _db.Products
+                       join pt in _db.Categories on p.CategoryId equals pt.CategoryId into join_p_pt
+                       from p_pt in join_p_pt.DefaultIfEmpty()
+                       join b in _db.Brands on p.BrandId equals b.BrandId into join_p_b
+                       from p_b in join_p_b.DefaultIfEmpty()
+
+                       select new PdVM
+                       {
+                           ProductId = p.ProductId,
+                           ProductName = p.ProductName,
+                           CategoryName = p_pt.CategoryName,
+                           BrandName = p_b.BrandName,
+                           ProductPrice = p.ProductPrice,
+                           ProductCost = p.ProductCost,
+                           ProductStock = p.ProductStock,
+                           Detail = p.Detail
+                       };
+
+            if (pdvm == null) return NotFound();
+            return View(pdvm);
+        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -43,6 +68,120 @@ namespace FrostyBear.Controllers
                 if (pdvm == null) return NotFound();
                 ViewBag.stext = stext;
                 return View(pdvm);
+            }
+        }
+
+        public IActionResult Crate()
+        {
+            ViewData["Categories"] = new SelectList(_db.Categories, "CategoryId", "CategoryName");
+            ViewData["Brand"] = new SelectList(_db.Brands, "BrandId", "BrandName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Products.Add(obj);  
+                    _db.SaveChanges(); 
+                    return RedirectToAction("Index"); 
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View(obj);
+            }
+            ViewBag.ErrorMessage = "การบันทึกผิดพลาด";
+            return View(obj);
+        }
+
+        public IActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                ViewBag.ErrorMessage = "ระบุ id";
+                return RedirectToAction("Index");
+            }
+            var obj = _db.Products.Find(id);
+            if (obj == null)
+            {
+                ViewBag.ErrorMessage = "ไม่พบข้อมูล";
+                return RedirectToAction("Index");
+            }
+            ViewData["Categories"] = new SelectList(_db.Categories, "CategoryId", "CategoryName",obj.ProductId);
+            ViewData["Brand"] = new SelectList(_db.Brands, "BrandId", "BrandName",obj.BrandId);
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Products.Update(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View(obj);
+            }
+            ViewBag.ErrorMessage = "การแก้ไขผิดพลาด";
+            ViewData["Categories"] = new SelectList(_db.Categories, "CategoryId", "CategoryName", obj.ProductId);
+            ViewData["Brand"] = new SelectList(_db.Brands, "BrandId", "BrandName", obj.BrandId);
+            return View(obj);
+        }
+
+        public IActionResult Delete(String id)
+        {
+            if (id == null)
+            {
+                ViewBag.ErrorMessage = "ระบุ id";
+                return RedirectToAction("Index");
+            }
+            var obj = _db.Products.Find(id);
+            if (obj == null)
+            {
+                ViewBag.ErrorMessage = "ไม่พบข้อมูล";
+                return RedirectToAction("Index");
+            }
+            ViewBag.pdtName = _db.Categories.FirstOrDefault(pt => pt.CategoryId == obj.CategoryId).CategoryName;
+            ViewBag.brandName = _db.Brands.FirstOrDefault(br => br.BrandId == obj.BrandId).BrandName;
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(string Pdid)
+        {
+            try
+            {
+                var obj = _db.Products.Find(Pdid);
+                if (obj == null)
+                {
+                    ViewBag.ErrorMessage = "ไม่พบข้อมูล";
+                    return RedirectToAction("Index");
+                }
+                _db.Products.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return RedirectToAction("Index");
             }
         }
     }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,24 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FrostyBear.Models;
 
 public partial class Product
 {
+    
     public int ProductId { get; set; }
 
+    [Required(ErrorMessage = "ต้องระบุชื่อสินค้า")]
+    [StringLength(100)]
+    [Display(Name = "ชื่อสินค้า")]
     public string ProductName { get; set; } = null!;
 
+    [Display(Name = "ราคา")]
     public decimal? ProductPrice { get; set; }
 
+    [Display(Name = "ประเภท")]
     public int? CategoryId { get; set; }
 
+    [Display(Name = "ยี่ห้อ")]
     public int? BrandId { get; set; }
 
+    [Display(Name = "รายละเอียดสินค้า")]
     public string? Detail { get; set; }
 
+    [Display(Name = "ต้นทุน")]
     public double? ProductCost { get; set; }
 
+    [Display(Name = "จำนวนคงเหลือ")]
     public int? ProductStock { get; set; }
 
     public virtual Brand? Brand { get; set; }
